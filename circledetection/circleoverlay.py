e696e51a83b5d53f0bd04e2ddab3668f041281b1
Lily Toledo <149692742+ELilyToledo@users.noreply.github.com>
2025-01-09 03:31:04 -0500

    Update circleoverlay.py

--- circledetection/circleoverlay.py
+++ circledetection/circleoverlay.py
@@ -1,1 +1,21 @@
+import cv2
+import numpy as np
+import math
 
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read() 
+
+    if not ret:
+        print("Error, cannot recieve stream")
+        break 
+    
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):  
+        break
+
+cap.release()
+cv2.destroyAllWindows()
