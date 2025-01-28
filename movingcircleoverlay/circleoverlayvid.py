57f0c5b0c38f0438f249b94a52e11c49c78b79d2
Lily Toledo <149692742+ELilyToledo@users.noreply.github.com>
2025-01-28 09:23:54 -0500

    Create circleoverlayvid.py

---
+++ movingcircleoverlay/circleoverlayvid.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+def circledetection(frame):
+    h,w,_ = frame.shape
+    aratio = w/h
+
+    #resizing the image by the aspect ratio
+    resize = cv2.resize(frame, (500, int(500 / aratio)))
+    copy = resize.copy()
+
+    #mask = np.zeros(img.shape[:2], dtype="uint8")
+
+    #applying grayscale and blurring the image
+    gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (7, 7), 0)
+    #cv2.imshow("blur",blur)
+
+    #finding circles using hough
+    circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, 900, param1=60, param2=110)
+
+    if circles is not None:
+        #rounding the values in the numpy array
+        circles = np.round(circles[0, :]).astype("int")
+        for circle in circles:
+            x,y,r = circle
+            cv2.circle(copy, (x,y), r, (0,255,0), 3)
+            cv2.circle(copy, (x, y), 1, (0, 255, 0), 2)
+            #cv2.circle(mask, (x, y), r, 255, -1)
+
+    #masked = cv2.bitwise_and(img, img, mask=mask)
+    #cv2.imshow("masked", mask)
+
+    return copy
+
+cap = cv2.VideoCapture("tube3crop.mp4")
+
+if (cap.isOpened() == False):
+    print("Cannot open video")
+
+while (cap.isOpened()):
+
+    ret, frame = cap.read()
+    if ret == True:
+
+        copy = circledetection(frame)
+        cv2.imshow('frame', copy)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
